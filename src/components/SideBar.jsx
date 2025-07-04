dea77ff0bfa864763ae4a2128635885ec59adfcb
mohammad31 <mohammad3haoaj31@gmail.com>
2025-07-04 23:40:01 +0300

    update project 1

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import Drawer from "@mui/material/Drawer";
+import {
+  IconButton,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  useTheme,
+} from "@mui/material";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import Divider from "@mui/material/Divider";
+import List from "@mui/material/List";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+
+const drawerWidth = 240;
+
+function SideBar({ open, DrawerHeader, handleDrawerClose }) {
+  const theme = useTheme();
+  return (
+    <Drawer
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        "& .MuiDrawer-paper": {
+          width: drawerWidth,
+          boxSizing: "border-box",
+        },
+      }}
+      variant="persistent"
+      anchor="left"
+      open={open}
+    >
+      <DrawerHeader>
+        <IconButton onClick={handleDrawerClose}>
+          {theme.direction === "ltr" ? (
+            <ChevronLeftIcon />
+          ) : (
+            <ChevronRightIcon />
+          )}
+        </IconButton>
+      </DrawerHeader>
+      <Divider />
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
+  );
+}
+
+export default SideBar;
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,7 +1,167 @@
-import React from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+  alpha,
+  Box,
+  IconButton,
+  Stack,
+  styled,
+  Toolbar,
+  useTheme,
+} from "@mui/material";
+import MuiAppBar from "@mui/material/AppBar";
+import SearchIcon from "@mui/icons-material/Search";
+import InputBase from "@mui/material/InputBase";
+import {
+  DarkModeOutlined,
+  LightModeOutlined,
+  NotificationsNone,
+  PersonOutlined,
+  SettingsOutlined,
+} from "@mui/icons-material";
 
-function TopBar() {
-  return <div></div>;
+const drawerWidth = 240;
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  variants: [
+    {
+      // @ts-ignore
+      props: ({ open }) => open,
+      style: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: `${drawerWidth}px`,
+        transition: theme.transitions.create(["margin", "width"], {
+          easing: theme.transitions.easing.easeOut,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+      },
+    },
+  ],
+}));
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
+
+function TopBar({ handleDrawerOpen, open, setmyMode }) {
+  const theme = useTheme();
+  return (
+    <AppBar
+      position="fixed"
+      // @ts-ignore
+      open={open}
+    >
+      <Toolbar>
+        {/* --------- menu icon ---------------------- */}
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          onClick={handleDrawerOpen}
+          edge="start"
+          sx={[
+            {
+              mr: 2,
+            },
+            open && { display: "none" },
+          ]}
+        >
+          <MenuIcon />
+        </IconButton>
+        {/* --------- serch input---------------------- */}
+        <Search>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+            placeholder="Search…"
+            inputProps={{ "aria-label": "search" }}
+          />
+        </Search>
+        {/* ------------------------------------------- */}
+        <Box flexGrow={1} />
+        {/* ------------------ icons ------------------------ */}
+        <Stack direction={"row"}>
+          {theme.palette.mode === "light" ? (
+            <IconButton
+              onClick={() => {
+                localStorage.setItem(
+                  "currentMode",
+                  theme.palette.mode === "light" ? "dark" : "light"
+                );
+                setmyMode(theme.palette.mode === "light" ? "dark" : "light");
+              }}
+              color={"inherit"}
+            >
+              <LightModeOutlined />
+            </IconButton>
+          ) : (
+            <IconButton
+              onClick={() => {
+                localStorage.setItem(
+                  "currentMode",
+                  theme.palette.mode === "light" ? "dark" : "light"
+                );
+                setmyMode(theme.palette.mode === "light" ? "dark" : "light");
+              }}
+              color={"inherit"}
+            >
+              <DarkModeOutlined />
+            </IconButton>
+          )}
+
+          <IconButton color={"inherit"}>
+            <NotificationsNone />
+          </IconButton>
+          <IconButton color={"inherit"}>
+            <SettingsOutlined />
+          </IconButton>
+          <IconButton color={"inherit"}>
+            <PersonOutlined />
+          </IconButton>
+        </Stack>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default TopBar;
