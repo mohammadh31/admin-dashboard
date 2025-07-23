466e3f6fafae3371813a50fa701541bbd1fe4399
mohammad31 <mohammad3haoaj31@gmail.com>
2025-07-23 14:49:02 +0300

    update project 6

--- src/pages/profileForm/ProfileForm.jsx
+++ src/pages/profileForm/ProfileForm.jsx
@@ -1,6 +1,20 @@
 import React from "react";
-import { Box, Button, MenuItem, Stack, TextField } from "@mui/material";
+import {
+  Alert,
+  Box,
+  Button,
+  MenuItem,
+  Snackbar,
+  Stack,
+  TextField,
+} from "@mui/material";
 import { useForm } from "react-hook-form";
+
+const regEmail =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+const phoneRegExp =
+  /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
 
 const data = [
   {
@@ -31,6 +45,20 @@ const ProfileForm = () => {
   } = useForm();
   // @ts-ignore
   const onSubmit = () => console.log("aaaaaaaaaaaaaaaaaaaaaaaaa");
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
 
   return (
     <Box
@@ -73,8 +101,32 @@ const ProfileForm = () => {
         />
       </Stack>
 
-      <TextField label="Email" variant="filled" />
-      <TextField label="Contact Number" variant="filled" />
+      <TextField
+        label="Email"
+        variant="filled"
+        helperText={
+          Boolean(errors.email) ? "Please provide a valid email address" : null
+        }
+        error={Boolean(errors.email)}
+        {...register("email", {
+          required: true,
+          pattern: regEmail,
+        })}
+      />
+      <TextField
+        label="Contact Number"
+        variant="filled"
+        helperText={
+          Boolean(errors.contactNumber)
+            ? "Please provide a valid phone number"
+            : null
+        }
+        error={Boolean(errors.contactNumber)}
+        {...register("contactNumber", {
+          required: true,
+          pattern: phoneRegExp,
+        })}
+      />
       <TextField label="Address 1" variant="filled" />
       <TextField label="Address 2" variant="filled" />
       <TextField
@@ -100,9 +152,25 @@ const ProfileForm = () => {
           type="submit"
           variant="contained"
           sx={{ textTransform: "capitalize" }}
+          onClick={handleClick}
         >
           Current New User
         </Button>
+        <Snackbar
+          anchorOrigin={{ vertical: "top", horizontal: "right" }}
+          open={open}
+          autoHideDuration={4000}
+          onClose={handleClose}
+        >
+          <Alert
+            onClose={handleClose}
+            severity="success"
+            variant="filled"
+            sx={{ width: "100%" }}
+          >
+            Account created successfully
+          </Alert>
+        </Snackbar>
       </Box>
     </Box>
   );
